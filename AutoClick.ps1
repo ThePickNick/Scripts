5203a33fa2a8ec13340a97d4e9036072991c3e53
ThePickNick <tomaszwida@gmail.com>
2016-12-12 15:31:43 -0500

    AutoClick
    AutoClick Script

---
+++ AutoClick.ps1
@@ -0,0 +1,16 @@
+﻿$wshell = New-Object -ComObject wscript.shell;
+
+$wshell.AppActivate('chrome.exe')
+
+$count= 0
+sleep 2
+
+
+while($count -ne 10000){
+Sleep 2
+$wshell.SendKeys('2')
+
+$count++
+
+Write-Output $count
+}
